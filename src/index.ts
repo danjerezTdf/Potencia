fb042a5960d3c8c829e6df944ec38f6efbac9db9
danjerezTdf <104039810+danjerezTdf@users.noreply.github.com>
2022-05-27 15:30:50 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
+/* Ejercicio: Potencias
+• Realice un programa que devuelva la potencia de un número.
+• La base y el exponente deben ser ingresados por teclado.
+• Sólo deben admitirse exponentes mayores o iguales a cero.
+• Recuerde que el resultado de un numero elevado a 0 es 1.
+• Separe la lógica de calcular la potencia utilizando métodos.
+*/
+
 let baseIN: number = Number(prompt(`Ingreser Valor de la Base:`));
 let exponenteIN: number = Number(prompt(`Ingreser valor del Exponente`));
 let potencia: number = 0;
